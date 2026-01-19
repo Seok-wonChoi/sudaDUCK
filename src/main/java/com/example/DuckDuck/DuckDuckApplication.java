9198fc639a79b96e548630555fd46fee7acf8780
장가은 <010810ok@naver.com>
2026-01-19 12:32:35 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/com/example/DuckDuck/DuckDuckApplication.java
@@ -0,0 +1,13 @@
+package com.example.DuckDuck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuckDuckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuckDuckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DuckDuck/DuckDuckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DuckDuck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuckDuckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
