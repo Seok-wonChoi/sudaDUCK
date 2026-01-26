dbb852958dd9541c74e33a63bb5bcd880211723b
이승엽 <axc5126@naver.com>
2026-01-26 17:42:01 +0900

    Update file main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import "./styles/index.css";
-const basename = import.meta.env.BASE_URL;
-ReactDOM.createRoot(document.getElementById("root")).render(
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+
+// [수정] 변수 대신 직접 '/dev'를 입력해서 테스트해 봅시다.
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* 개발 서버인 경우 '/dev'를 강제로 설정 */}
+    <BrowserRouter basename="/dev">
       <App />
     </BrowserRouter>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
